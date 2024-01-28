111b54994eadb85e2955df85edf6973b0342bfa4
Marbeck <raphael@watchnext.fr>
2024-01-28 18:34:08 +0100

    Day 1 OK

---
+++ day1/src/main/scala/Main.scala
@@ -0,0 +1,41 @@
+import scala.io.Source
+import com.typesafe.scalalogging.Logger
+
+val loggerAOC = Logger("aoc")
+val loggerAOCPart1 = Logger("aoc.part1")
+val loggerAOCPart2 = Logger("aoc.part2")
+
+@main def hello: Unit =
+  loggerAOC.trace("Root trace activated")
+  loggerAOC.debug("Root debug activated")
+  println("Launching Day1")
+  List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
+    val (score1, score2) = f.apply()
+    println(s"1 : ${score1}")
+    println(s"2 : ${score2}")
+    println(s"----------------")
+  println("Done")
+
+object Solver:
+  def runOn(inputLines: Seq[String]): (String, String) =
+
+    val result = inputLines.head.scanLeft(0):
+      case (acc, '(') => acc + 1
+      case (acc, ')') => acc - 1
+
+    val result1 = s"${result.last}"
+    val result2 = s"${result.indexWhere(_ == -1)}"
+
+    (s"${result1}", s"${result2}")
+
+  def solveTest: (String, String) =
+    solver("test.txt")
+  def solve: (String, String) =
+    solver("data.txt")
+  private def solver(fileName: String): (String, String) =
+    val bufferedSource = Source.fromResource(fileName)
+    val lines = bufferedSource.getLines().toSeq
+    lines match
+      case Nil => ("", "")
+      case _ => runOn(lines)
+end Solver
