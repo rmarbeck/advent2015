e2395b913417c3d20d9174dbd78a6a36932f32b5
rmarbeck <rafoufou>
2024-01-29 11:46:18 +0100

    Day 4 OK

---
+++ day4/src/main/scala/Main.scala
@@ -0,0 +1,71 @@
+import scala.io.Source
+import com.typesafe.scalalogging.Logger
+import scala.collection.parallel.CollectionConverters.*
+
+
+val loggerAOC = Logger("aoc")
+val loggerAOCPart1 = Logger("aoc.part1")
+val loggerAOCPart2 = Logger("aoc.part2")
+
+@main def hello: Unit =
+  loggerAOC.trace("Root trace activated")
+  loggerAOC.debug("Root debug activated")
+  println("Launching Day4")
+  List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
+    val (score1, score2) = f.apply()
+    println(s"1 : ${score1}")
+    println(s"2 : ${score2}")
+    println(s"----------------")
+  println("Done")
+
+object Solver:
+  def runOn(inputLines: Seq[String]): (String, String) =
+
+    //println((0 to Int.MaxValue).takeWhile(index => MD5.hash(inputLines.head + index.toString).take(5) != "00000" ).last + 1)
+    //println((0 to Int.MaxValue).takeWhile(index => MD5.hash(inputLines.head + index.toString).take(6) != "000000").last + 1)
+
+    val (resultPart1: Int, resultPart2: Int) =
+      startsWith(6, inputLines.head).foldLeft((0, 0)):
+        case (acc, value) => value == 5 match
+          case true if acc(0) == 0 => (acc(1), acc(1) + 1)
+          case _ => (acc(0), acc(1) + 1)
+
+    val result1 = s"${resultPart1}"
+    val result2 = s"${resultPart2}"
+
+    (s"${result1}", s"${result2}")
+
+  def solveTest: (String, String) =
+    solver("test.txt")
+  def solve: (String, String) =
+    solver("data.txt")
+  private def solver(fileName: String): (String, String) =
+    val bufferedSource = Source.fromResource(fileName)
+    val lines = bufferedSource.getLines().toSeq
+    lines match
+      case Nil => ("", "")
+      case _ => runOn(lines)
+end Solver
+
+def startsWith(numberOfZeros: Int, key: String, index: Int = 0): LazyList[Int] =
+  val computed = MD5.hash(key + index.toString).take(numberOfZeros)
+  computed.take(numberOfZeros) == "0"*numberOfZeros match
+    case true => LazyList.empty
+    case false => LazyList.cons(computed.indexWhere(_ != '0'), startsWith(numberOfZeros, key, index+1))
+
+
+def startsWith2(numberOfZeros: Int, key: String, index: Int = 0): LazyList[Int] =
+  val computed = MD5.hash(key + index.toString).take(numberOfZeros)
+  computed.indexWhere(_ != '0') match
+    case value if value == numberOfZeros => LazyList.empty
+    case value => LazyList.cons(value, startsWith(numberOfZeros, key, index+1))
+
+
+object MD5 {
+  def hash(s: String) = {
+    val m = java.security.MessageDigest.getInstance("MD5")
+    val b = s.getBytes("UTF-8")
+    m.update(b, 0, b.length)
+    new java.math.BigInteger(1, m.digest()).toString(16).reverse.padTo(32, "0").reverse.mkString
+  }
+}
