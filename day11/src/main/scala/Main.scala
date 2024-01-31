824c046d6553db64189cc13531da59583f538cda
rmarbeck <rafoufou>
2024-01-31 11:51:38 +0100

    Day 11 OK but can be speeded up

--- day11/src/main/scala/Main.scala
+++ day11/src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 import scala.io.Source
 import com.typesafe.scalalogging.Logger
 
+import scala.annotation.tailrec
+
 val loggerAOC = Logger("aoc")
 val loggerAOCPart1 = Logger("aoc.part1")
 val loggerAOCPart2 = Logger("aoc.part2")
@@ -18,8 +20,13 @@ val loggerAOCPart2 = Logger("aoc.part2")
 
 object Solver:
   def runOn(inputLines: Seq[String]): (String, String) =
-    val result1 = s""
-    val result2 = s""
+
+    val resultPart1 = findNextValidPassword(inputLines.head)
+    val resultPart2 = findNextValidPassword(resultPart1.password)
+
+
+    val result1 = s"${resultPart1.password}"
+    val result2 = s"${resultPart2.password}"
 
     (s"${result1}", s"${result2}")
 
@@ -33,4 +40,46 @@ object Solver:
     lines match
       case Nil => ("", "")
       case _ => runOn(lines)
+
+  def findNextValidPassword(from: String): Password =
+    def passwordLazyList(from: String): LazyList[Password] =
+      val newValue = Password(from).next
+      newValue #:: passwordLazyList(newValue.password)
+    passwordLazyList(from).find(_.isValid).get
+
+
+  val forbidden = Array('i', 'o', 'l')
+  val forbiddenAsInt = forbidden.map(_.toInt)
+  val alphabet = ('a' to 'z').toArray
+
+  val aAsInt = 'a'.toInt
+  val zAsInt = 'z'.toInt
+
+  case class Password(password: String):
+    def isValid: Boolean = doesNotContainForbidden && containsThreeIncreasingLetter && containsTwoDifferentPairs
+
+    def next: Password =
+      @tailrec
+      def findNext(current: String, toAdd: Int = 0): String =
+        current.length match
+          case 0 => "a" * toAdd
+          case _ =>
+            val (start, last) = (current.init, current.last)
+            last match
+              case 'z' => findNext(start, toAdd + 1)
+              case value => start + alphabet(alphabet.indexOf(value) + 1) + "a" * toAdd
+
+      Password(findNext(password))
+
+    private def doesNotContainForbidden: Boolean = password.intersect(forbidden).isEmpty
+
+    private def containsThreeIncreasingLetter: Boolean =
+      def matches(chars: Array[Char]): Boolean =
+        chars.map(alphabet.indexOf(_)).sliding(2, 1).forall(value => value(1) == value(0) + 1)
+
+      password.sliding(3, 1).map(_.toCharArray).exists(matches)
+
+    private def containsTwoDifferentPairs: Boolean = alphabet.map(_.toString * 2).count(password.contains(_)) >= 2
+
 end Solver
+
