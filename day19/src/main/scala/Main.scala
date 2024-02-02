d449d755516430e613f860af95064fedd747b9ec
rmarbeck <rafoufou>
2024-02-02 07:52:12 +0100

    Day 19 intiated

---
+++ day19/src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+import scala.io.Source
+import com.typesafe.scalalogging.Logger
+
+val loggerAOC = Logger("aoc")
+val loggerAOCPart1 = Logger("aoc.part1")
+val loggerAOCPart2 = Logger("aoc.part2")
+
+@main def hello: Unit =
+  loggerAOC.trace("Root trace activated")
+  loggerAOC.debug("Root debug activated")
+  println("Launching Day19")
+  List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
+    val (score1, score2) = f.apply()
+    println(s"1 : ${score1}")
+    println(s"2 : ${score2}")
+    println(s"----------------")
+  println("Done")
+
+object Solver:
+  def runOn(inputLines: Seq[String]): (String, String) =
+
+    val (replacements, molecule) = inputLines.filterNot(_.isEmpty).foldLeft((List[Replacement](), "")):
+      case (acc, newLine) =>
+        newLine match
+          case s"$from => $to" => (Replacement(from, to) :: acc(0), acc(1))
+          case value => (acc(0), value)
+
+    var results = Set[String]()
+
+    replacements.foreach:
+      case replacement => molecule.
+
+    val result1 = s""
+    val result2 = s""
+
+    (s"${result1}", s"${result2}")
+
+  def solveTest: (String, String) =
+    solver("test.txt")
+  def solve: (String, String) =
+    solver("data.txt")
+  private def solver(fileName: String): (String, String) =
+    val bufferedSource = Source.fromResource(fileName)
+    val lines = bufferedSource.getLines().toSeq
+    lines match
+      case Nil => ("", "")
+      case _ => runOn(lines)
+end Solver
+
+case class Replacement(from: String, to: String)
