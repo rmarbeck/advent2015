e72106c923c758d1093e0dd33b8e6346ac62f28f
rmarbeck <rafoufou>
2024-11-06 17:24:59 +0100

    Even shorter and cleaner

--- day8/src/main/scala/Main.scala
+++ day8/src/main/scala/Main.scala
@@ -3,30 +3,30 @@ import com.typesafe.scalalogging.Logger
 
 import scala.annotation.tailrec
 
-val loggerAOC = Logger("aoc")
-val loggerAOCPart1 = Logger("aoc.part1")
-val loggerAOCPart2 = Logger("aoc.part2")
+val loggerAOC: Logger = Logger("aoc")
+val loggerAOCPart1: Logger = Logger("aoc.part1")
+val loggerAOCPart2: Logger = Logger("aoc.part2")
 
-@main def hello: Unit =
+@main def hello(): Unit =
   loggerAOC.trace("Root trace activated")
   loggerAOC.debug("Root debug activated")
   println("Launching Day8")
   List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
     val (score1, score2) = f.apply()
-    println(s"1 : ${score1}")
-    println(s"2 : ${score2}")
+    println(s"1 : $score1")
+    println(s"2 : $score2")
     println(s"----------------")
   println("Done")
 
 object Solver:
   def runOn(inputLines: Seq[String]): (String, String) =
 
-    val List(standardLength, lessLength, moreLength) = inputLines.map(current => (current.length, less(current), more(current))).unzip3.toList.map(_.sum)
+    val List(standardLength, lessLength, moreLength) = inputLines.map(compute).unzip3.toList.map(_.sum)
 
     val result1 = s"${standardLength - lessLength}"
     val result2 = s"${moreLength - standardLength}"
 
-    (s"${result1}", s"${result2}")
+    (s"$result1", s"$result2")
 
   def solveTest: (String, String) =
     solver("test.txt")
@@ -39,46 +39,32 @@ object Solver:
       case Nil => ("", "")
       case _ => runOn(lines)
 
-  @tailrec
-  private def less(rawString: String, numberOfAntiSlash: Int = 0, current: Int = 0): Int =
-    rawString.length match
-      case 0 => current
-      case _ =>
-        val (head, tail) = (rawString.head, rawString.tail)
-        numberOfAntiSlash match
-          case 0 =>
-            head match
-              case '\\' => less(tail, 1, current)
-              case '"' => less(tail, 0, current)
-              case _ => less(tail, 0, current + 1)
-          case 1 =>
-            head match
-              case '\\' => less(tail, 0, current + 1)
-              case '"' => less(tail, 0, current + 1)
-              case 'x' => less(tail, 2, current)
-              case _ => less(tail, 0, current + 1)
-          case 2 => less(tail, 3, current)
-          case 3 => less(tail, 0, current + 1)
+  private def compute(line: String): (Int, Int, Int) =
+    val chars = line.toList
+    (chars.length, less2(chars), more2(chars))
 
   @tailrec
-  private def more(rawString: String, numberOfAntiSlash: Int = 0, current: Int = 0): Int =
-    rawString.length match
-      case 0 => current
-      case _ =>
-        val (head, tail) = (rawString.head, rawString.tail)
-        numberOfAntiSlash match
-          case 0 =>
-            head match
-              case '\\' => more(tail, 1, current)
-              case '"' => more(tail, 0, current + 3)
-              case _ => more(tail, 0, current + 1)
-          case 1 =>
-            head match
-              case '\\' => more(tail, 0, current + 4)
-              case '"' => more(tail, 0, current + 4)
-              case 'x' => more(tail, 0, current + 3)
-              case _ => throw Exception("Not supported")
+  private def less2(rawString: List[Char], numberOfAntiSlash: Int = 0, current: Int = 0): Int =
+    rawString match
+      case Nil => current
+      case head :: tail =>
+        (numberOfAntiSlash, head) match
+          case (0, '\\') => less2(tail, 1, current)
+          case (0, '"') => less2(tail, 0, current)
+          case (1, 'x') => less2(tail, 2, current)
+          case (2, _) => less2(tail, 3, current)
+          case _ => less2(tail, 0, current + 1)
+
+  @tailrec
+  private def more2(rawString: List[Char], numberOfAntiSlash: Int = 0, current: Int = 0): Int =
+    rawString match
+      case Nil => current
+      case head :: tail =>
+        (numberOfAntiSlash, head) match
+          case (0, '\\') => more2(tail, 1, current)
+          case (0, '"') | (1, 'x') => more2(tail, 0, current + 3)
+          case (0, _) => more2(tail, 0, current + 1)
+          case (1, '\\' | '"') => more2(tail, 0, current + 4)
+          case _ => throw Exception("Not supported")
 
 end Solver
-
-
