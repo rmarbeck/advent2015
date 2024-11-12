c9d7f1a9b530808e5ea8ce96828237cb25b45c66
rmarbeck <rafoufou>
2024-11-12 17:41:51 +0100

    Small cleaning

--- day5/src/main/scala/Main.scala
+++ day5/src/main/scala/Main.scala
@@ -3,31 +3,30 @@ import com.typesafe.scalalogging.Logger
 
 import scala.annotation.tailrec
 
-val loggerAOC = Logger("aoc")
-val loggerAOCPart1 = Logger("aoc.part1")
-val loggerAOCPart2 = Logger("aoc.part2")
+val loggerAOC: Logger = Logger("aoc")
+val loggerAOCPart1: Logger = Logger("aoc.part1")
+val loggerAOCPart2: Logger = Logger("aoc.part2")
 
-@main def hello: Unit =
+@main def hello(): Unit =
   loggerAOC.trace("Root trace activated")
   loggerAOC.debug("Root debug activated")
   println("Launching Day5")
   List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
     val (score1, score2) = f.apply()
-    println(s"1 : ${score1}")
-    println(s"2 : ${score2}")
+    println(s"1 : $score1")
+    println(s"2 : $score2")
     println(s"----------------")
   println("Done")
 
 object Solver:
-  def runOn(inputLines: Seq[String]): (String, String) =
+  private def runOn(inputLines: Seq[String]): (String, String) =
 
-    val resultPart1 = inputLines.count(NiceString(_).isNicePart1)
-    val resultPart2 = inputLines.count(NiceString(_).isNicePart2)
+    val List(resultPart1, resultPart2) = inputLines.map(NiceString.apply).map(current => (current.isNicePart1, current.isNicePart2)).unzip.toList.map(_.count(identity))
 
     val result1 = s"$resultPart1"
     val result2 = s"$resultPart2"
 
-    (s"${result1}", s"${result2}")
+    (s"$result1", s"$result2")
 
   def solveTest: (String, String) =
     solver("test.txt")
@@ -41,8 +40,8 @@ object Solver:
       case _ => runOn(lines)
 end Solver
 
-val vowels = List('a', 'e', 'i', 'o', 'u')
-val forbidden = List("ab", "cd", "pq", "xy")
+val vowels: Seq[Char] = List('a', 'e', 'i', 'o', 'u')
+val forbidden: Seq[String] = List("ab", "cd", "pq", "xy")
 
 case class NiceString(input: String):
   private def contains3Vowels: Boolean = input.count(vowels.contains) >= 3
@@ -53,22 +52,22 @@ case class NiceString(input: String):
     @tailrec
     def findPair(inString: String): Boolean =
       inString match
-        case value if value.size < 4 => false
+        case value if value.length < 4 => false
         case value =>
           val (start, pseudoTail) = value.splitAt(2)
-          pseudoTail.contains(start) match
-            case true => true
-            case false => findPair(inString.tail)
+          if pseudoTail.contains(start) then
+            true
+          else
+            findPair(inString.tail)
     findPair(input)
   private def containsOneLetterSeparatedByOne: Boolean =
     @tailrec
     def findLetter(inString: String): Boolean =
       inString match
-        case value if value.size < 3 => false
+        case value if value.length < 3 => false
         case value if value(0) == value(2) => true
         case _ => findLetter(inString.tail)
     findLetter(input)
-
 
   def isNicePart1: Boolean = contains3Vowels && containsDouble && doesNotContainForbidden
 
