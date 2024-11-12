4ca73cef211b8fdca4862246600b13927e477cfc
rmarbeck <rafoufou>
2024-11-12 16:11:26 +0100

    Very small cleaning

--- day4/src/main/scala/Main.scala
+++ day4/src/main/scala/Main.scala
@@ -3,34 +3,33 @@ import com.typesafe.scalalogging.Logger
 import scala.collection.parallel.CollectionConverters.*
 
 
-val loggerAOC = Logger("aoc")
-val loggerAOCPart1 = Logger("aoc.part1")
-val loggerAOCPart2 = Logger("aoc.part2")
+val loggerAOC: Logger = Logger("aoc")
+val loggerAOCPart1: Logger = Logger("aoc.part1")
+val loggerAOCPart2: Logger = Logger("aoc.part2")
 
-@main def hello: Unit =
+@main def hello(): Unit =
   loggerAOC.trace("Root trace activated")
   loggerAOC.debug("Root debug activated")
   println("Launching Day4")
   List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
     val (score1, score2) = f.apply()
-    println(s"1 : ${score1}")
-    println(s"2 : ${score2}")
+    println(s"1 : $score1")
+    println(s"2 : $score2")
     println(s"----------------")
   println("Done")
 
 object Solver:
-  def runOn(inputLines: Seq[String]): (String, String) =
+  private def runOn(inputLines: Seq[String]): (String, String) =
 
     val (resultPart1: Int, resultPart2: Int) =
       startsWith(6, inputLines.head).foldLeft((0, 0)):
-        case (acc, value) => value == 5 match
-          case true if acc(0) == 0 => (acc(1), acc(1) + 1)
-          case _ => (acc(0), acc(1) + 1)
+        case (acc, 5) if acc._1 == 0 => (acc._2, acc._2 + 1)
+        case (acc, _) => (acc._1, acc._2 + 1)
 
-    val result1 = s"${resultPart1}"
-    val result2 = s"${resultPart2}"
+    val result1 = s"$resultPart1"
+    val result2 = s"$resultPart2"
 
-    (s"${result1}", s"${result2}")
+    (s"$result1", s"$result2")
 
   def solveTest: (String, String) =
     solver("test.txt")
@@ -50,12 +49,12 @@ def startsWith(numberOfZeros: Int, key: String, index: Int = 0): LazyList[Int] =
     case value => LazyList.cons(value, startsWith(numberOfZeros, key, index+1))
 
 
-object MD5 {
-  def hashNumberOfLeadingZerosBasedOnFirstBytes(s: String, maxZerosToFind: Int): Int = {
+object MD5:
+  def hashNumberOfLeadingZerosBasedOnFirstBytes(s: String, maxZerosToFind: Int): Int =
     val nbOfBytes = math.ceil(maxZerosToFind / 2d).toInt
     val m = java.security.MessageDigest.getInstance("MD5")
     m.update(s.getBytes)
-    val firstBytes = m.digest.take(maxZerosToFind/2)
+    val firstBytes = m.digest.take(nbOfBytes)
     val valueOfFirstBytes = firstBytes.foldLeft(0):
       case (acc, currentByte) => acc * 256 + (currentByte & 0xff).toInt
 
@@ -63,15 +62,14 @@ object MD5 {
       case 0 => maxZerosToFind
       case value if value < 16 => maxZerosToFind - 1
       case _ => -1
-  }
 
-  def hashNumberOfLeadingZeros(s: String): Int = {
+  def hashNumberOfLeadingZeros(s: String): Int =
     val m = java.security.MessageDigest.getInstance("MD5")
     m.update(s.getBytes)
     val valueAsBytes = m.digest
     32 - new java.math.BigInteger(1, valueAsBytes).toString(16).length
-  }
-  def hash(s: String): String = {
+
+  def hash(s: String): String =
     val m = java.security.MessageDigest.getInstance("MD5")
     val b = s.getBytes("UTF-8")
     m.update(b, 0, b.length)
@@ -79,5 +77,3 @@ object MD5 {
     32-result.length match
       case 0 => result
       case value => "0"*value + result
-  }
-}
