89723d595160a61ad9a42119c66c916b2d23d358
rmarbeck <rafoufou>
2024-11-13 14:07:16 +0100

    New implementation to improve performances and avoid using last on list

--- day1/src/main/scala/Main.scala
+++ day1/src/main/scala/Main.scala
@@ -1,32 +1,47 @@
 import scala.io.Source
 import com.typesafe.scalalogging.Logger
 
-val loggerAOC = Logger("aoc")
-val loggerAOCPart1 = Logger("aoc.part1")
-val loggerAOCPart2 = Logger("aoc.part2")
+import scala.annotation.targetName
 
-@main def hello: Unit =
+val loggerAOC: Logger = Logger("aoc")
+val loggerAOCPart1: Logger = Logger("aoc.part1")
+val loggerAOCPart2: Logger = Logger("aoc.part2")
+
+@main def hello(): Unit =
   loggerAOC.trace("Root trace activated")
   loggerAOC.debug("Root debug activated")
   println("Launching Day1")
   List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
     val (score1, score2) = f.apply()
-    println(s"1 : ${score1}")
-    println(s"2 : ${score2}")
+    println(s"1 : $score1")
+    println(s"2 : $score2")
     println(s"----------------")
   println("Done")
 
 object Solver:
-  def runOn(inputLines: Seq[String]): (String, String) =
+  private def runOn(inputLines: Seq[String]): (String, String) =
 
-    val result = inputLines.head.scanLeft(0):
+    /*val result = inputLines.head.scanLeft(0):
       case (acc, '(') => acc + 1
-      case (acc, ')') => acc - 1
+      case (acc, _) => acc - 1
 
     val result1 = s"${result.last}"
-    val result2 = s"${result.indexWhere(_ == -1)}"
+    val result2 = s"${result.indexWhere(_ == -1)}"*/
 
-    (s"${result1}", s"${result2}")
+    /**
+     * Solution avoiding passing through all elements (or a great number of elements) of resulting list
+     */
+
+    import scala.language.postfixOps
+    val result = inputLines.head.foldLeft(Counter.Empty):
+      case (counter, ')') => counter --
+      case (counter, '(') => counter ++
+      case (counter, unknownChar) => throw Exception(s"Not recognized character : $unknownChar")
+
+    val result1 = s"${result.level}"
+    val result2 = s"${result.part2Index}"
+
+    (s"$result1", s"$result2")
 
   def solveTest: (String, String) =
     solver("test.txt")
@@ -39,3 +54,15 @@ object Solver:
       case Nil => ("", "")
       case _ => runOn(lines)
 end Solver
+
+case class Counter(level: Int, unmatchedIndex: Option[Int], index: Int):
+  lazy val part2Index: Int = unmatchedIndex.getOrElse(-1)
+  @targetName("plusOne")
+  def ++ : Counter = this.copy(level + 1, index = index + 1)
+  @targetName("minusOne")
+  def -- : Counter = level - 1 match
+    case -1 if unmatchedIndex.isEmpty => this.copy(-1, Some(index), index = index + 1)
+    case _ => this.copy(level - 1, index = index + 1)
+
+object Counter:
+  val Empty: Counter = Counter(0, None, 1)
